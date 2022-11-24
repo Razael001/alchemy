6584eb112e87b9b00f99220866c7510c4f40379d
Razael001 <77176187+Razael001@users.noreply.github.com>
2022-11-24 10:21:58 +0100

    first commit

--- index.js
+++ index.js
@@ -1,36 +1,106 @@
-const express = require('express')
+const { Network, Alchemy } = require('alchemy-sdk');
 const mongoose = require('mongoose')
+const dotenv = require("dotenv");
+dotenv.config();
 const cors = require('cors')
-const app = express();
-const Messages = require("./models/messageModel")
-const path = require('path')
-
+const Data = require("./models/dataModel");
+const express = require('express')
+const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb+srv://azeez:Raza2003@cluster0.1rnci.mongodb.net/portfolio?retryWrites=true&w=majority");
+//Connecting to mongoDb
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.post("/api/sendmessage", async(req, res) => {
+const settings = {
+  apiKey: process.env.API_KEY,
+  network: Network.ETH_MAINNET,
+};
+
+const alchemy = new Alchemy(settings);
+
+// Main function
+app.post("/api/getData", async (req, res) => {
+
+  //Getting the whole transactions details
+  const data = await alchemy.core.getAssetTransfers({
+    category: ["external", "internal", "erc20", "erc721", "erc1155"],
+  });
+
+  //Setting variables
+  let max = data.transfers[0].value;
+  let maxArray = []
+  let sum = 0
+
+  //Getting the transfer with the max value 
+  for (let i = 0; i < data.transfers.length; i++) {
+    if (data.transfers[i].value > max) {
+      max = data.transfers[i].value;
+      maxArray = data.transfers[i]
+    }
+  }
+
+  //Getting the total sum of transaction 
+  for (let i = 0; i < data.transfers.length; i++) {
+    if (data.transfers[i].value > 0 && i <= 1500) {
+      sum = (sum * 1) + (data.transfers[i].value * 1)
+    }
+  }
+
+
+  //Saving in the database
   try {
-    await Messages.create({
-      name: req.body.name,
-      email: req.body.email,
-      message: req.body.message
+
+    //Checking if a data exists for the token already
+    const tokenData = await Data.findOne({
+      tokenAddress: "ETH_MAINNET"
     })
-    res.json({status: "message sent", sent: true })
+
+    //If the token's data exists already
+    if (tokenData) {
+      const newData = await Data.findOneAndUpdate(
+        {
+          tokenAddress: "ETH_MAINNET"
+        },
+        {
+          sentMost: maxArray.from,
+          receivedMost: maxArray.to,
+          totalAmount: sum
+        })
+    }
+
+    //If the token's data does not exist already
+    else {
+      const newData = await Data.create({
+        tokenAddress: "ETH_MAINNET",
+        sentMost: maxArray.from,
+        receivedMost: maxArray.to,
+        totalAmount: sum
+      })
+    }
   }
-  catch(err) {
-    res.json({status: "not sent", sent: false})
+  catch (err) {
+    res.json(err)
   }
+
+  //Returns
+
+  res.json({
+    sendsMost: maxArray.from,
+    receivedMost: maxArray.to,
+    SumOfLatestTransactions: sum
+  })
+
 })
-
-app.use(express.static(path.join(__dirname, '/client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
 
 app.listen(process.env.PORT || '1564', () => {
-  console.log("connected successful")
+  console.log("connected to server successful")
 })
+
+//Calling the function
